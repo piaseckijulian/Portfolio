04dc9edcdc111bfb8a7730c2ebcf28762a4c4f6e
Julian Piasecki <julian.piasecki@outlook.com>
2023-11-02 00:29:37 +0100

    Updating grid

--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -10,7 +10,7 @@ const TechStack = async () => {
       <h2 className="text-center text-4xl font-bold text-white xs:text-5xl">
         Tech Stack
       </h2>
-      <div className="my-10 grid grid-cols-3 place-items-center gap-3 xs:grid-cols-6 xs:gap-4 md:grid-cols-11">
+      <div className="my-10 grid grid-cols-3 place-items-center gap-3 xs:grid-cols-6 xs:gap-4 md:grid-cols-12">
         {techStackItems.map(({ alt, image }: ImageInterface) => (
           <Image src={urlFor(image).url()} alt={alt} width={64} height={64} key={alt} />
         ))}
