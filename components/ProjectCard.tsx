278e25064a208c1097abeeac3b8405333f8f1ff3
Julian Piasecki <julian.piasecki@outlook.com>
2024-05-19 23:51:48 +0200

    style: consistent project description height

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -28,25 +28,25 @@ const ProjectCard = ({
   ];
 
   return (
-    <div className="border-2 border-zinc-800 text-white">
+    <div className="flex flex-col border-2 border-zinc-800 text-white">
       <Image
         src={urlForImage(image)}
         alt={`Picture of the ${title}`}
-        width={500}
-        height={500}
-        className="w-full object-contain"
+        width={384}
+        height={183}
+        className="w-full object-cover"
       />
 
-      <div className="my-5 px-5">
-        <h3 className="mb-3 text-center text-2xl font-semibold xs:text-3xl lg:text-4xl">
+      <div className="my-5 flex h-full w-full flex-col gap-3 px-5">
+        <h3 className="text-center text-2xl font-semibold xs:text-3xl lg:text-4xl">
           {title}
         </h3>
 
-        <p className="xs:text-left xs:text-lg">{desc}</p>
+        <p className="flex-1 xs:text-left xs:text-lg">{desc}</p>
 
-        <p className="my-3 text-sm">Tech stack: {techStack.join(', ')}</p>
+        <p className="text-sm">Tech stack: {techStack.join(', ')}</p>
 
-        <div className=" flex flex-col gap-x-6 gap-y-2 lg:flex-row">
+        <div className="flex flex-col gap-x-6 gap-y-2 lg:flex-row">
           {links.map(({ text, href, icon: Icon }) => (
             <Link
               href={href}
@@ -60,6 +60,8 @@ const ProjectCard = ({
           ))}
         </div>
       </div>
+
+      {/* </div> */}
     </div>
   );
 };
